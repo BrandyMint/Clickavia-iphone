620c09bb90dd4c8fc6e751bed2c884e5e3e264a3
denisdbv <denisdbv@gmail.com>
2013-08-22 12:37:47 +0400

    Инициализация CAFieldComplete

--- clickavia-iphone/Controllers/MainScreenViewController/MainScreenViewController.h
+++ clickavia-iphone/Controllers/MainScreenViewController/MainScreenViewController.h
@@ -7,10 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CAFieldComplete/CAFieldCompleteView.h>
+#import <CAManagers/CitiesManager.h>
 #import <CACalendarView/CACalendarView.h>
 #import "CACalendarMockDates.h"
 
 @interface MainScreenViewController : UIViewController
+
+@property (strong,nonatomic) IBOutlet CAFieldCompleteView* departureCompleteView;
+@property (strong,nonatomic) IBOutlet CAFieldCompleteView* returnCompleteView;
+@property (strong,nonatomic) CitiesManager *cm;
+@property (strong,nonatomic) Destination *departureDestination;
+@property (strong,nonatomic) Destination *returnDestination;
 
 @property (strong,nonatomic) IBOutlet CACalendarView* calendarView;
 
--- clickavia-iphone/Controllers/MainScreenViewController/MainScreenViewController.m
+++ clickavia-iphone/Controllers/MainScreenViewController/MainScreenViewController.m
@@ -29,6 +29,15 @@
     
     self.navigationController.navigationBarHidden = YES;
     
+    _cm = [CitiesManager new];
+    _cm.delay = 10;
+    _departureDestination = [Destination new];
+    _returnDestination = [Destination new];
+    _departureCompleteView.isReturn = NO;
+    _departureCompleteView.delegate = self;
+    _returnCompleteView.delegate = self;
+    _returnCompleteView.isReturn = YES;
+    
     [_calendarView setDelegate:self];
     [_calendarView selectFlyToDaysByDateArray:[CACalendarMockDates generateFlyToDates]];
     [_calendarView selectFlyReturnDaysByDateArray:[CACalendarMockDates generateFlyReturnDates:[NSDate date]]];
@@ -52,6 +61,64 @@
 {
     return nil;
 }
+
+#pragma mark CAFieldCompleteViewDelegate
+- (void)fieldCompleteView:(CAFieldCompleteView*) fieldCompleteView selectedDestination:(Destination*) destination
+{
+    if(fieldCompleteView==_departureCompleteView)
+    {
+        _departureDestination = destination;
+        [_cm getDestinationsForReturn:@"" forDepartureDestination:_departureDestination completeBlock:^(NSArray *array)
+         {
+             [_returnCompleteView setAutocompleteData:array];
+         }];
+    }
+    else
+    {
+        _returnDestination = destination;
+    }
+}
+
+- (void)fieldCompleteView:(CAFieldCompleteView*) fieldCompleteView textChanged:(NSString*) text
+{
+    if(fieldCompleteView == _departureCompleteView)
+    {
+        [_cm getDestinationsForDeparture:text completeBlock:^(NSArray *array)
+         {
+             [_departureCompleteView setAutocompleteData:array];
+         }];
+    }
+    if(fieldCompleteView == _returnCompleteView)
+    {
+        [_cm getDestinationsForReturn:text forDepartureDestination:_departureDestination completeBlock:^(NSArray *array)
+         {
+             [_returnCompleteView setAutocompleteData:array];
+         }];
+    }
+}
+-(void)fieldCompleteViewNeedFront:(CAFieldCompleteView *)fieldCompleteView
+{
+    [self.view bringSubviewToFront:fieldCompleteView];
+}
+-(void)fieldCompleteViewBeginEditing:(CAFieldCompleteView *)fieldCompleteView
+{
+    if(fieldCompleteView == _departureCompleteView)
+    {
+        [_cm getDestinationsForDeparture:_departureCompleteView.text completeBlock:^(NSArray *array)
+         {
+             [_departureCompleteView setAutocompleteData:array];
+         }];
+    }
+    if(fieldCompleteView == _returnCompleteView)
+    {
+        [_cm getDestinationsForReturn:_returnCompleteView.text forDepartureDestination:_departureDestination completeBlock:^(NSArray *array)
+         {
+             [_returnCompleteView setAutocompleteData:array];
+         }];
+    }
+}
+
+#pragma mark CACalendarViewDelegate
 
 - (void) calendarView:(CACalendarView*)calendarView didSelectDate:(NSDate*)date
 {
